8c1eb1ce7d76e0e13580ee5c21385bac35ccd70f
SadhanaSri <59409994+SadhanaSri@users.noreply.github.com>
2021-03-02 18:14:36 +0530

    Update Program9.java

--- day11/Program9.java
+++ day11/Program9.java
@@ -1,40 +1,50 @@
 package day11;
 
-import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Program9 {
 	
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
 		int n=1;
 		int mark;
 		int sum=0;
 		
-		Program8 obj = new Program8();
+		Program9 obj = new Program9();
 		
-			do {
-				try {
-					n++;
-					mark = obj.getInput();
-					sum += mark;
-				} catch(InputMismatchException e) {
-					n = n-1;
-					System.out.println("Enter a valid integer value");
-					continue;
-				}
-			} while(n <= 10);	
+		do {
+			n++;
+			mark = obj.getInput();				
+			
+			try {
+			    if(mark < 0)  
+				throw new NegativeInputException("Please enter a positive number");
+			    else if(mark > 100)
+				throw new LimitExceededException("Please enter a value below 100");
+			 } catch(Exception e) {
+			     n = n-1;
+			 }
+		         
+			 sum += mark;
+		} while(n <= 10);	
 			
 		System.out.println("Average mark of 10 students: "+sum/10);
-		sc.close();
 	}
 	
-	int getInput() {
+	public int getInput() {
 		Scanner sc = new Scanner(System.in);
 		int num =  sc.nextInt();
-		sc.close();
 		return num;
 	}
 }
 
-	
+class NegativeInputException extends Exception {
+	public NegativeInputException(String msg) {
+		System.out.println(msg);
+	}
+}
+
+class LimitExceededException extends Exception {
+	public LimitExceededException(String msg) {
+		System.out.println(msg);
+	}
+}
