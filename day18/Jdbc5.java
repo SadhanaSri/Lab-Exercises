33b4fb5594bb5e0ecae4bb84fbf0030d3729c1f5
SadhanaSri <59409994+SadhanaSri@users.noreply.github.com>
2021-03-15 20:11:05 +0530

    Update Jdbc5.java

--- day18/Jdbc5.java
+++ day18/Jdbc5.java
@@ -7,7 +7,7 @@ import java.sql.DriverManager;
 public class Jdbc5 {
 	public static void main(String[] args) throws Exception {
 		 Class.forName("com.mysql.cj.jdbc.Driver"); 
-		 Connection con = DriverManager.getConnection("jdbc:mysql://localhost/ey", "root", "@sadhanakalyan1822");
+		 Connection con = DriverManager.getConnection("jdbc:mysql://localhost/ey", "root", "");
 		 Statement st = con.createStatement();
 		 st.execute("drop table coffee");
 	}
