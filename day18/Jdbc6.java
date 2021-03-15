69e6d8820e75bc1314b4c3c05e4ff272c9af36c0
SadhanaSri <59409994+SadhanaSri@users.noreply.github.com>
2021-03-15 20:11:25 +0530

    Update Jdbc6.java

--- day18/Jdbc6.java
+++ day18/Jdbc6.java
@@ -7,7 +7,7 @@ import java.util.Enumeration;
 public class Jdbc6 {
 	public static void main(String[] args) throws Exception {
 		 Class.forName("com.mysql.cj.jdbc.Driver"); 
-		 Connection con = DriverManager.getConnection("jdbc:mysql://localhost/ey", "root", "@sadhanakalyan1822");
+		 Connection con = DriverManager.getConnection("jdbc:mysql://localhost/ey", "root", "");
 		 
 		 Enumeration e = DriverManager.getDrivers();
 		 System.out.println(" "+DriverManager.getLoginTimeout());
