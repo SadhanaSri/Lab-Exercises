0bec929ef28bc6d741bd8bbbb6ed738a03a2adb7
SadhanaSri <59409994+SadhanaSri@users.noreply.github.com>
2021-03-24 15:13:57 +0530

    Update LoginAction.java

--- servlet assignment/mvc/src/model/LoginAction.java
+++ servlet assignment/mvc/src/model/LoginAction.java
@@ -4,47 +4,61 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import dao.DBUtility;
 
-public class LoginAction implements Action{
+public class LoginAction extends Action{
 	Connection con;
 	PreparedStatement st;
-	RequestDispatcher rd;
+	String result;
 	
 	@Override
 	public String execute(HttpServletRequest request, HttpServletResponse response) {
 		try {
-			Connection con = DBUtility.getConnection();
-			PreparedStatement st = con.prepareStatement("select * from users where uname=?");
-		
-			String name = request.getParameter("name");
+			con = DBUtility.getConnection();
+			st = con.prepareStatement("select * from users where uname=?");
+			
+			String name=request.getParameter("name");
+			String pass=request.getParameter("pass");
+			
+			HttpSession session=request.getSession();
+			session.setAttribute("uname", name);
+			session.setAttribute("upass", pass);
+			
 			st.setString(1, name);
 			ResultSet rs = st.executeQuery();
 		
 			if(rs.next() == false) {
-				return "login.failure";	
+				result =  "login.failure";	
 			}
 			else {
 				int flag = rs.getInt("flag");
-				System.out.println("flag..........."+flag);
-				if(flag == 0) {
-					PreparedStatement update = con.prepareStatement("update users set flag=1 where uname=?");
-					update.setString(1, name);
-					System.out.println(update.executeUpdate());
+				String upass = rs.getString("upass");
+				
+				if(upass.equals(pass)) {
+					if(flag == 0) {
+						PreparedStatement update = con.prepareStatement("update users set flag=1 where uname=?");
+						update.setString(1, name);
+						if(update.executeUpdate() == 1) 
+							result = "login.success";
+						else 
+							result = "login.failure";	
+					} else {
+						result = "login.stay";
+					}
 				} else {
-					return "login.stay";
-				}
+					result =  "login.failure";	
+				}				
 			}
 			DBUtility.closeConnection(null);
-			return "login.success";
+			return result;
 		} catch (Exception e) {
 			DBUtility.closeConnection(e);
 			e.printStackTrace();
-			return "login.failure";
+			return result;
 		}
 	}
 }
