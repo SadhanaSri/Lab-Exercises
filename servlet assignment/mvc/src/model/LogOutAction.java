adffa834ba4dfbe937ed74375403746568869d99
SadhanaSri <59409994+SadhanaSri@users.noreply.github.com>
2021-03-24 15:14:19 +0530

    Update LogOutAction.java

--- servlet assignment/mvc/src/model/LogOutAction.java
+++ servlet assignment/mvc/src/model/LogOutAction.java
@@ -6,27 +6,34 @@ import java.sql.SQLException;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import dao.DBUtility;
 
-public class LogOutAction implements Action {
+public class LogOutAction extends Action {
 	Connection con;
 	PreparedStatement st;
+	String result;
 	
 	@Override
 	public String execute(HttpServletRequest request, HttpServletResponse response) {
 		try {
-			String name = request.getParameter("name");
+			HttpSession session=request.getSession();
+			String name=(String)session.getAttribute("uname");
+			
 			con = DBUtility.getConnection();
 			st = con.prepareStatement("update users set flag=0 where uname=?");
 			st.setString(1, name);
-			st.executeUpdate();
+			if(st.executeUpdate() == 1)
+				result = "logout.success";
+			else
+				result = "logout.failure";
 			DBUtility.closeConnection(null);
-			return "logout.success";
+			return result;
 		} catch (SQLException e) {
 			DBUtility.closeConnection(e);
 			e.printStackTrace();
-			return "logout.failure";
+			return result;
 		}
 	}
 }
