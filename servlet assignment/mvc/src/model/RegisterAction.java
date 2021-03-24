ab254c5b0d2c70f453bab9c7cde6dea7a7bf9d12
SadhanaSri <59409994+SadhanaSri@users.noreply.github.com>
2021-03-24 15:13:28 +0530

    Update RegisterAction.java

--- servlet assignment/mvc/src/model/RegisterAction.java
+++ servlet assignment/mvc/src/model/RegisterAction.java
@@ -7,38 +7,46 @@ import java.sql.SQLException;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import dao.DBUtility;
 
-public class RegisterAction implements Action {
+public class RegisterAction extends Action {
 	Connection con;
 	PreparedStatement st;
+	String result;
 	
 	@Override
 	public String execute(HttpServletRequest request, HttpServletResponse response) {
-		String name = request.getParameter("name");
-		String pass = request.getParameter("pass");
-		try {
-			con = DBUtility.getConnection();
-			st = con.prepareStatement("select count(*) from users");
-			ResultSet rs = st.executeQuery();
-			int count=0;
-			
-			while(rs.next()) {
+		HttpSession session=request.getSession();
+		String name=(String)session.getAttribute("uname");
+		String pass = (String)session.getAttribute("upass");
+		
+		if(pass.length() != 0) {
+			try {
+				con = DBUtility.getConnection();
+				st = con.prepareStatement("select count(*) from users");
+				ResultSet rs = st.executeQuery();
+				int count=0;
+				
 				count = rs.getInt(1);
-			}
-			
-			st = con.prepareStatement("insert into users values (?, ?, ?, 0)");
-			st.setInt(1, ++count);
-			st.setString(2, name);
-			st.setString(3, pass);
-			System.out.println(st.executeUpdate());
-			DBUtility.closeConnection(null);
-			return "register.success";
-		} catch (SQLException e) {
-			DBUtility.closeConnection(e);
-			e.printStackTrace();
-			return "register.failure";
-		}	
+				
+				st = con.prepareStatement("insert into users values (?, ?, ?, 0)");
+				st.setInt(1, ++count);
+				st.setString(2, name);
+				st.setString(3, pass);
+				st.executeUpdate();
+				
+				DBUtility.closeConnection(null);
+				result = "register.success";
+			} catch (SQLException e) {
+				DBUtility.closeConnection(e);
+				e.printStackTrace();
+				result = "register.failure";
+			}	
+		} else {
+			result = "register.failure";
+		}
+		return result;
 	}
 }
